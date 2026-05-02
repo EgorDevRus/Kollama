3b35fc98e80dd8219c915d5c62df8b554bbd0335
EgorDevRus <egor.rus.edu@gmail.com>
2026-05-02 17:50:57 +0300

    feat: add Constants.kt with base URL and model name

---
+++ app/src/main/java/com/kollama/app/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.kollama.app.utils
+
+/**
+ * Константы приложения
+ */
+object Constants {
+    /** Адрес локального сервера Ollama для эмулятора Android */
+    const val BASE_URL = "http://10.0.2.2:11434"
+
+    /** Название модели нейросети по умолчанию */
+    const val DEFAULT_MODEL = "llama3:latest"
+}
